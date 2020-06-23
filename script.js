6863a77fb143cb53ea0b7be098ba2ad38d199bf6
Rose Egle <rosiemegle89@gmail.com>
2020-06-23 15:25:43 -0400

    got rid of drop table button changed chart to image instead of interactive

--- script.js
+++ script.js
@@ -1,93 +1,93 @@
 
-am4core.ready(function () {
+// am4core.ready(function () {
 
-    // Themes begin
-    am4core.useTheme(am4themes_spiritedaway);
-    am4core.useTheme(am4themes_animated);
-    // Themes end
+//     // Themes begin
+//     am4core.useTheme(am4themes_spiritedaway);
+//     am4core.useTheme(am4themes_animated);
+//     // Themes end
 
-    var chart = am4core.create("chartdiv", am4charts.PieChart);
-    chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
+//     var chart = am4core.create("chartdiv", am4charts.PieChart);
+//     chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
-    let currentBudget = 63821448
-    let efc = 64332327
-    let overage = efc - currentBudget
-    let totalExposure = 957948
-    let exposure = totalExposure - overage
-    let budgetDouble = currentBudget * 2
-    let nonOverage = budgetDouble - efc
-
-
-    console.log("Overage", overage)
-    console.log("Exposure", exposure)
-
-    chart.data = [{
-        "category": "Total Commited",
-        "dollars": 60712420,
-        "color": am4core.color("#749D98"),
-        "align": false,
-    }, {
-        "category": "Pending",
-        "dollars": 2662869,
-        "color": am4core.color("#AF805E"),
-        "align": true,
-    }, {
-        "category": "Exposure",
-        "dollars": exposure,
-        "color": am4core.color("#F35033"),
-        "align": true,
-    }, {
-        "category": "Exposure Overage",
-        "dollars": overage,
-        "color": am4core.color("#AA445B"),
-        "align": true,
-    }];
-
-    //connects data to chart
-    var series = chart.series.push(new am4charts.PieSeries());
-    series.dataFields.value = "dollars";
-    series.dataFields.category = "category";
-
-    //creates shape of pie chart
-    chart.radius = am4core.percent(60);
-    chart.innerRadius = am4core.percent(30);
-    chart.startAngle = 180;
-    chart.endAngle = 361;
-
-    //creates radial gradient
-    var rgm = new am4core.RadialGradientModifier();
-    rgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
-    series.slices.template.fillModifier = rgm;
+//     let currentBudget = 63821448
+//     let efc = 64332327
+//     let overage = efc - currentBudget
+//     let totalExposure = 957948
+//     let exposure = totalExposure - overage
+//     let budgetDouble = currentBudget * 2
+//     let nonOverage = budgetDouble - efc
 
 
-    //creates rounded corner raidius
-    series.slices.template.cornerRadius = 10;
-    series.slices.template.innerCornerRadius = 7;
+//     console.log("Overage", overage)
+//     console.log("Exposure", exposure)
 
-    //mouse interactivity
-    series.slices.template.draggable = true;
-    series.slices.template.inert = true;
+//     chart.data = [{
+//         "category": "Total Commited",
+//         "dollars": 60712420,
+//         "color": am4core.color("#749D98"),
+//         "align": false,
+//     }, {
+//         "category": "Pending",
+//         "dollars": 2662869,
+//         "color": am4core.color("#AF805E"),
+//         "align": true,
+//     }, {
+//         "category": "Exposure",
+//         "dollars": exposure,
+//         "color": am4core.color("#F35033"),
+//         "align": true,
+//     }, {
+//         "category": "Exposure Overage",
+//         "dollars": overage,
+//         "color": am4core.color("#AA445B"),
+//         "align": true,
+//     }];
 
-    //aligns labels in columns
-    series.alignLabels = "align";
-    series.labels.template.fontSize = 18;
+//     //connects data to chart
+//     var series = chart.series.push(new am4charts.PieSeries());
+//     series.dataFields.value = "dollars";
+//     series.dataFields.category = "category";
+
+//     //creates shape of pie chart
+//     chart.radius = am4core.percent(60);
+//     chart.innerRadius = am4core.percent(30);
+//     chart.startAngle = 180;
+//     chart.endAngle = 361;
+
+//     //creates radial gradient
+//     var rgm = new am4core.RadialGradientModifier();
+//     rgm.brightnesses.push(-0.8, -0.8, -0.5, 0, - 0.5);
+//     series.slices.template.fillModifier = rgm;
 
 
+//     //creates rounded corner raidius
+//     series.slices.template.cornerRadius = 10;
+//     series.slices.template.innerCornerRadius = 7;
 
+//     //mouse interactivity
+//     series.slices.template.draggable = true;
+//     series.slices.template.inert = true;
 
-    // series.slices.template.propertyFields.fill = "color";
-    series.slices.template.stroke = am4core.color("#fff");
-    series.slices.template.strokeWidth = 2;
-    series.slices.template.strokeOpacity = 1;
+//     //aligns labels in columns
+//     series.alignLabels = "align";
+//     series.labels.template.fontSize = 18;
 
 
 
-    series.hiddenState.properties.startAngle = 90;
-    series.hiddenState.properties.endAngle = 90;
+
+//     // series.slices.template.propertyFields.fill = "color";
+//     series.slices.template.stroke = am4core.color("#fff");
+//     series.slices.template.strokeWidth = 2;
+//     series.slices.template.strokeOpacity = 1;
 
 
 
-}); // end am4core.ready()
+//     series.hiddenState.properties.startAngle = 90;
+//     series.hiddenState.properties.endAngle = 90;
+
+
+
+// }); // end am4core.ready()
 
 let smoothScroll = (target, duration) => {
     var target = document.querySelector(target)
